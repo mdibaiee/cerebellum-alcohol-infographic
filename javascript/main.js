49df3568e3ab9dac26e0d2f7173beb6f7f2bff80
Mahdi Dibaiee <mdibaiee@pm.me>
2021-10-11 19:13:00 +0100

    basic animation

---
+++ javascript/main.js
@@ -0,0 +1,38 @@
+anime({
+  targets: '#human',
+  opacity: 1,
+  duration: 2000,
+  easing: 'easeInOutExpo'
+});
+
+anime({
+  targets: '#brain',
+  opacity: 1,
+  duration: 2000,
+  delay: 1000,
+  easing: 'easeInOutExpo'
+});
+
+anime({
+  targets: '#brain-projection',
+  opacity: 1,
+  duration: 2000,
+  delay: 1000,
+  easing: 'easeInOutExpo'
+});
+
+anime({
+  targets: '#Alcohol',
+  opacity: 1,
+  duration: 2000,
+  delay: 2000,
+  easing: 'easeInOutExpo'
+});
+
+anime({
+  targets: '#connection-rectangle',
+  opacity: 1,
+  duration: 1000,
+  delay: 3000,
+  easing: 'easeInOutExpo'
+});
