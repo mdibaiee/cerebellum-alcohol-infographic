11d12b9ac3e7fe704481186ebf7cd988b707b9c8
Mahdi Dibaiee <mdibaiee@pm.me>
2021-10-15 18:03:31 +0100

    add basic chart for alcohol abuse global

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,27 @@
+// Alcohol Abuse Chart
+
+const alcoholAbuseCanvas = document.getElementById('alcohol-abuse');
+const alcoholDeathPercent = alcoholAbuseData.Deaths.filter(function(a) { return a.metric == 'Percent' });
+const alcoholPrevalencePercent = alcoholAbuseData.Prevalence.filter(function(a) { return a.metric == 'Percent' });
+const alcoholAbuseChart = new Chart(alcoholAbuseCanvas, {
+  type: 'line',
+  data: {
+    labels: alcoholDeathPercent.map(function(a) { return a.year }),
+    datasets: [{
+      label: 'Deaths by Alcohol Abuse',
+      data: alcoholDeathPercent.map(function(a) { return a.val }),
+      fill: true,
+      backgroundColor: 'rgb(249, 98, 200)'
+    }, {
+      label: 'Prevalence of Alcohol Abuse',
+      data: alcoholPrevalencePercent.map(function(a) { return a.val }),
+      borderColor: 'rgb(97, 152, 250)'
+    }]
+  }
+});
+
+// Animations
+
 anime({
   targets: '#human',
   opacity: 1,
